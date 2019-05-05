b906713f925c6eb2486207fb5dacccbbb2fa44e7
AJ Guiles <ashiguiles@gmail.com>
2019-05-05 19:48:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,8 +25,9 @@ class Scraper
   
 
   def self.scrape_profile_page(profile_url)
-    tudent = {}
-    profile_page = Nokogiri::HTML(open(profile_slug))
+    student = {}
+    profile_page = Nokogiri::HTML(open(profile_url))
+    
     links = profile_page.css(".social-icon-container").children.css("a").map { |el| el.attribute('href').value}
     links.each do |link|
       if link.include?("linkedin")
