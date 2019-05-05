37c1b5e7b4f2a9d8d818da5f90b86568b886abe1
AJ Guiles <ashiguiles@gmail.com>
2019-05-05 18:56:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,16 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    index_url = open("./fixtures/student-site/index.html")
+    
+    doc = nokogiri::HTML(index_url)
+    
+    # Scraper.scrape_index_page(index_url)
+    
     
   end
 
